b540d004b1dbb3cd13b135b10fc121b1dee69a6c
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2015-12-14 13:31:54 +0500

    index js

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+//= third-party/typed.js
+
+$(function(){
+    $(".gamestyped").typed({
+        strings: ["футбол", "баскетбол", "волейбол"],
+        typeSpeed: 170,
+        backDelay: 600,
+        loop: true,
+        loopCount: false
+    });
+});
