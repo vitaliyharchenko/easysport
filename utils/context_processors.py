91fcba6503feaded2622c489d987c7de086e17b2
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2015-12-16 23:28:51 +0500

    notifications init

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,4 +1,6 @@
 from users.models import User
+from notifications.models import Notification
+from django.utils import timezone
 
 
 def loggedin_user(request):
@@ -11,4 +13,26 @@ def loggedin_user(request):
             context['current_user'] = None
     except User.DoesNotExist:
         pass
+    return context
+
+
+def notifications(request):
+    context = dict()
+    query = Notification.objects.filter(user_id=request.user.id, datetime__lte=timezone.now())
+    new_count = query.filter(read=0).count()
+    read_count = query.filter(read=1).count()
+    if new_count:
+        new = query.filter(read=0)
+        context['notifications_new'] = new
+    else:
+        new = Notification.objects.none()
+
+    if read_count:
+        read = query.filter(read=1)
+    else:
+        read = Notification.objects.none()
+
+    context['notifications_all'] = new | read
+
+    context['notifications_count'] = new_count + read_count
     return context
