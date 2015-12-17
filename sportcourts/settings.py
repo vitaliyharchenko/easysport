26b78ab85a8a9d6c33b914a334e075fd934cce27
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2015-12-17 20:23:28 +0500

    some production changes

--- sportcourts/settings.py
+++ sportcourts/settings.py
@@ -138,15 +138,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
-]
+if IS_PRODUCTION:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = '/opt/sportcourts2/static/'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = '/static/'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
 
 MEDIA_URL = '/media/'
 
