3bf5a40eb7e90609b7e9ee518d8aee55787db4ea
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2015-12-19 19:27:34 +0500

    permissions changes

--- games/admin.py
+++ games/admin.py
@@ -14,14 +14,28 @@ class UserGameActionInline(admin.TabularInline):
 class GameAdmin(admin.ModelAdmin):
     model = Game
     inlines = [UserGameActionInline]
-    exclude = ('created_by', 'sporttype',)
+    exclude = ('sporttype',)
+    readonly_fields = ('created_by',)
     list_display = ('id', 'datetime', 'gametype', 'court', 'capacity', 'responsible_user', 'created_by', 'is_reported')
 
     def has_add_permission(self, request):
         return permissions.admin_organizer_permissions(request)
 
     def has_change_permission(self, request, obj=None):
-        return permissions.admin_organizer_responsible_permissions(request)
+        if request.user:
+            if request.user.is_admin or request.user.is_organizer or request.user.is_responsible:
+                if obj is None:
+                    return True
+                else:
+                    if obj.responsible_user == request.user or obj.created_by == request.user or request.user.is_admin or request.user.is_superuser:
+                        return True
+                    else:
+                        return False
+            else:
+                return False
+        else:
+            return False
+        # return permissions.admin_organizer_permissions(request)
 
     def has_module_permission(self, request):
         return permissions.admin_organizer_permissions(request)
--- utils/permissions.py
+++ utils/permissions.py
@@ -1,23 +1,13 @@
 def only_admin_permissions(request):
-    if request and request.user.is_admin:
+    if request.user and request.user.is_admin:
         return True
     else:
         return False
 
 
 def admin_organizer_permissions(request):
-    if request:
+    if request.user:
         if request.user.is_admin or request.user.is_organizer:
-            return True
-        else:
-            return False
-    else:
-        return False
-
-
-def admin_organizer_responsible_permissions(request):
-    if request:
-        if request.user.is_admin or request.user.is_organizer or request.user.is_responsible:
             return True
         else:
             return False
