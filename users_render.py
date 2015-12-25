6c5b5e8e01bec7942f6d0cd9df576104acee0cbd
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2015-12-26 00:30:45 +0500

    users migration for avatars

--- users_render.py
+++ users_render.py
@@ -12,6 +12,11 @@ django.setup()
 
 from users.models import User
 
+from urllib.request import urlopen
+from io import BytesIO
+from django.core.files import File
+import hashlib
+
 users = User.objects.all()
 for user in users:
     password = user.password
@@ -19,4 +24,21 @@ for user in users:
     user.is_active = True
     user.save()
     user.last_login = timezone.now()
+    user.save()
+
+    avatar_url = u'http://sportcourts.ru/images/avatars/{}'.format(user.pk)
+    response = urlopen(avatar_url)
+    io = BytesIO(response.read())
+    file = File(io)
+    str = file.readlines()
+
+    hashId = hashlib.md5()
+    hashId.update(repr(str).encode('utf-8'))
+    hash = hashId.hexdigest()
+
+    if hash == '0a70ab0f26836cb1c759346d897493c9':
+        print(u'False for user id:{}'.format(user.pk))
+    else:
+        user.avatar.save(u'avatar{}'.format(user.pk), file)
+
     user.save()
