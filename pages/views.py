30c8a93fdec2748f7c7a4bb80257cff48f4efd67
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2016-01-04 16:33:21 +0500

    production ready index

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from users.models import User
 
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'index.html')
+    users = User.objects.filter(pk__in=[1, 94, 90, 41, 29, 28, 14, 842])
+    return render(request, 'index.html', {'users': users})
 
 
 def contacts_view(request):
