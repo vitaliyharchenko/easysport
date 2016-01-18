f62c64caeef763b04e22d0fd97bb1489175a6f7e
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2016-01-18 12:41:51 +0500

    permissions on user searching

--- users/admin.py
+++ users/admin.py
@@ -30,8 +30,8 @@ class UserAdmin(UserAdmin):
     def has_change_permission(self, request, obj=None):
         return permissions.only_admin_permissions(request)
 
-    def has_view_permission(self, request):
-        return permissions.admin_organizer_responsible_permissions(request)
+    # def has_view_permission(self, request):
+    #     return permissions.admin_organizer_responsible_permissions(request)
 
     def has_module_permission(self, request):
         return permissions.admin_organizer_responsible_permissions(request)
